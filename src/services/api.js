2092f03b44b76243d67fcc7875a2c07f43c3e899
douglas <douglasfmr1@gmail.com>
2021-11-18 16:25:09 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`)
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const requestJson = await request.json();
-  return requestJson
+  return requestJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const requestJson = await request.json();
-  return requestJson
+  return requestJson;
 }
