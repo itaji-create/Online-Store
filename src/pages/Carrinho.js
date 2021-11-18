68ec04a42068ac2354fd5e83bfc393f22e0f4684
Itaji Carvalho <itaji.1999@gmail.com>
2021-11-18 16:48:22 -0300

    lint

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
 class Carrinho extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-       
-    }
-    }
-
   render() {
     return (
       <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-    )
+    );
   }
 }
 export default Carrinho;
