b6c9b24d8749eb2d7a66fb4ff2b3f28f7e0d9524
Itaji Carvalho <itaji.1999@gmail.com>
2021-11-22 14:14:05 -0300

    adiciona o carrinho e 'buttons' para alterar a quantidade e remover produtos

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -6,6 +6,34 @@ class Carrinho extends React.Component {
     this.state = {
       carrinho: JSON.parse(localStorage.getItem('cartItems')),
     };
+    this.deleteCard = this.deleteCard.bind(this);
+    this.changeQtd = this.changeQtd.bind(this);
+  }
+
+  deleteCard({ target }) {
+    const { carrinho } = this.state;
+    const name = target.parentNode.parentNode.firstChild.innerText;
+    let i = carrinho.findIndex((card) => card.id === name);
+    carrinho.splice(i, 1);
+    this.setState({ carrinho });
+    localStorage.setItem('cartItems', JSON.stringify([...carrinho]));
+  }
+
+  changeQtd({ target }) {
+    const operator = target.id;
+    const { carrinho } = this.state;
+    const name = target.parentNode.parentNode.firstChild.innerText;
+    let i = carrinho.findIndex((e) => e.id === name);
+
+    if (operator === '+') {
+      carrinho[i].qtd += 1;
+      this.setState({ carrinho });
+      localStorage.setItem('cartItems', JSON.stringify([...carrinho]));
+    } else {
+      if (carrinho[i].qtd >= 1) carrinho[i].qtd -= 1;
+      this.setState({ carrinho });
+      localStorage.setItem('cartItems', JSON.stringify([...carrinho]));
+    }
   }
 
   render() {
@@ -22,11 +50,36 @@ class Carrinho extends React.Component {
                 {item.title}
               </h3>
               <img src={ item.thumbnail } alt={ item.title } />
-              <p data-testid="shopping-cart-product-quantity">1</p>
+              <p><i>R$: </i>{ item.price }</p>
+              <div>
+                <p data-testid="shopping-cart-product-quantity"><i>Qtd: </i>{ item.qtd }</p>
+                <button
+                  type="button"
+                  id="+"
+                  onClick={ this.changeQtd }
+                  data-testid="product-increase-quantity"
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  id="-"
+                  onClick={ this.changeQtd }
+                  data-testid="product-decrease-quantity"
+                >
+                  -
+                </button>
+                <button type="button" onClick={ this.deleteCard }>remover</button>
+              </div>
             </div>
           ))
         )
           : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        <div>
+          <h2>Resumo do pedido</h2>
+          <h3>Total</h3>
+          <button type="button">Continuar</button>
+        </div>
       </div>
     );
   }
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Proptypes from 'prop-types';
 import { getProductsById } from '../services/api';
-import Products from '../components/Products';
 
 class ProductDetail extends React.Component {
   constructor(props) {
@@ -15,22 +14,28 @@ class ProductDetail extends React.Component {
     };
     this.productDetails = this.productDetails.bind(this);
     this.saveCartItems = this.saveCartItems.bind(this);
+    this.setSaveCard = this.setSaveCard.bind(this);
   }
 
   componentDidMount() {
     this.productDetails();
   }
 
-  saveCartItems = () => {
+  setSaveCard() {
     const { productInfo: { title, thumbnail, price }, id } = this.state;
-    const result = { title, thumbnail, price, id };
-    if (localStorage.getItem('cartItems')) {
-      const readLocalProducts = JSON.parse(localStorage.getItem('cartItems'));
-      localStorage.setItem('cartItems', JSON.stringify([...readLocalProducts, result]));
+    const result = { title, thumbnail, price, id, qtd: 1 };
+    const readLocalProducts = JSON.parse(localStorage.getItem('cartItems'));
+    localStorage.setItem('cartItems', JSON.stringify([...readLocalProducts, result]));
+  }
+
+  saveCartItems = () => {
+    const { id } = this.state;
+    const localKeys = JSON.parse(localStorage.getItem('cartItems'));
+    if (localKeys) {
+      if (localKeys.every((e) => e.id !== id)) this.setSaveCard();
     } else {
       localStorage.setItem('cartItems', JSON.stringify([]));
-      const readLocalProducts = JSON.parse(localStorage.getItem('cartItems'));
-      localStorage.setItem('cartItems', JSON.stringify([...readLocalProducts, result]));
+      this.setSaveCard();
     }
   };
 
@@ -50,21 +55,34 @@ class ProductDetail extends React.Component {
         title,
         thumbnail,
         price,
-        id,
       },
       load,
     } = this.state;
     return (
-      <div data-testid="product-detail-name">
+      <div>
         <Link data-testid="shopping-cart-button" to="/carrinho">Carrinho</Link>
         {load
-          && <Products
-            id={ id }
-            title={ title }
-            image={ thumbnail }
-            price={ price }
-            handleClick={ this.saveCartItems }
-          />}
+          && (
+            <div className="product" data-testid="product">
+              <h2
+                data-testid="product-detail-name"
+              >
+                { title }
+              </h2>
+              <img src={ thumbnail } alt={ title } />
+              <p>
+                R$:
+                { price }
+              </p>
+              <button
+                type="button"
+                data-testid="product-detail-add-to-cart"
+                onClick={ this.saveCartItems }
+              >
+                Adicionar ao carrinho
+              </button>
+            </div>
+          )}
       </div>
     );
   }
